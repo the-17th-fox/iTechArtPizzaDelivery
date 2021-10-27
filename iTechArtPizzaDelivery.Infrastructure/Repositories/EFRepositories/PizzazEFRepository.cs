215e4e25c0a0a222f7285d7781956643d13f3a71
the-17th-fox <nikitos171002@yandex.by>
2021-10-27 23:15:38 +0300

    Add new Pizzas rep. (EF)

---
+++ iTechArtPizzaDelivery.Infrastructure/Repositories/EFRepositories/PizzazEFRepository.cs
@@ -0,0 +1,19 @@
+﻿using iTechArtPizzaDelivery.Domain.Entities;
+using iTechArtPizzaDelivery.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArtPizzaDelivery.Infrastructure.Repositories.EFRepositories
+{
+    public class PizzazEFRepository
+    {
+        private readonly PizzaDeliveryContext context;
+
+        public PizzazEFRepository (PizzaDeliveryContext context) => this.context = context;
+
+        public List<Pizza> GetPizzasInfo() => context.Pizzas.ToList();
+    }
+}
