b67407353a7edc014a1f53d0607d971cb7de2917
the-17th-fox <nikitos171002@yandex.by>
2021-10-27 23:14:42 +0300

    Add Context.PizzaDeliveryContext to Infrastructure

---
+++ iTechArtPizzaDelivery.Infrastructure/Context/PizzaDeliveryContext.cs
@@ -0,0 +1,24 @@
+﻿using iTechArtPizzaDelivery.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArtPizzaDelivery.Infrastructure.Context
+{
+    public class PizzaDeliveryContext : DbContext
+    {
+        public DbSet<Pizza> Pizzas { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Ingredient> Ingredients { get; set; }
+        public DbSet<PromoCode> PromoCodes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer(connectionString);
+        }
+
+    }
+}
