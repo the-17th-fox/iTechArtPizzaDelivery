31c8c21d567ac59ebb041039216aee534addfa6c
the-17th-fox <nikitos171002@yandex.by>
2021-10-27 23:11:46 +0300

    Add Pizzas Service
    Needs to be finished, look for todo-info

---
+++ iTechArtPizzaDelivery.Domain/Services/PizzasService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iTechArtPizzaDelivery.Domain.Interfaces;
+
+namespace iTechArtPizzaDelivery.Domain.Services
+{
+    public class PizzasService
+    {
+        private readonly IPizzasRepository _pizzasRepository;
+        public PizzasService(IPizzasRepository context)
+        {
+            _pizzasRepository = context;
+        }
+
+        // TODO: Add GetAll and PizasServ Interface 
+    }
+}
