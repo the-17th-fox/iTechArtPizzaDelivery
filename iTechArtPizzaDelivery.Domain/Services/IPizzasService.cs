106702965e3ccc7473cdc8f490deb01ab5b24015
the-17th-fox <nikitos171002@yandex.by>
2021-10-28 12:47:51 +0300

    Add IPizzasService interface and it's implementation in PizzasService

---
+++ iTechArtPizzaDelivery.Domain/Services/IPizzasService.cs
@@ -0,0 +1,16 @@
+﻿using iTechArtPizzaDelivery.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTechArtPizzaDelivery.Domain.Services
+{
+    public interface IPizzasService
+    {
+        public List<Pizza> GetAll();
+        public Pizza FindById(ulong id);
+        public void Post(string name, string description);
+    }
+}
--- iTechArtPizzaDelivery.Domain/Services/PizzasService.cs
+++ iTechArtPizzaDelivery.Domain/Services/PizzasService.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iTechArtPizzaDelivery.Domain.Entities;
 using iTechArtPizzaDelivery.Domain.Interfaces;
 
 namespace iTechArtPizzaDelivery.Domain.Services
 {
-    public class PizzasService
+    public class PizzasService : IPizzasService
     {
         private readonly IPizzasRepository _pizzasRepository;
         public PizzasService(IPizzasRepository context)
@@ -15,6 +16,19 @@ namespace iTechArtPizzaDelivery.Domain.Services
             _pizzasRepository = context;
         }
 
-        // TODO: Add GetAll and PizasServ Interface 
+        public List<Pizza> GetAll()
+        {
+            return _pizzasRepository.GetAllPizzas();
+        }
+
+        public Pizza FindById(ulong id)
+        {
+            return _pizzasRepository.FindPizzaById(id);
+        }
+
+        public void Post(string name, string description)
+        {
+            _pizzasRepository.PostPizza(name, description);
+        }
     }
 }
