baa7e8b910a34eefc783fb596c49a574f1a3a06e
the-17th-fox <nikitos171002@yandex.by>
2021-10-28 20:25:06 +0300

    Changes due to old project renaming

--- iTechArtPizzaDelivery.Infrastructure/Repositories/Fakes/PizzasFakeRepository.cs
+++ iTechArtPizzaDelivery.Infrastructure/Repositories/Fakes/PizzasFakeRepository.cs
@@ -1,71 +1,68 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using iTechArtPizzaDelivery.Domain.Entities;
-using iTechArtPizzaDelivery.Domain.Interfaces;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using iTechArtPizzaDelivery.Domain.Entities;
+//using iTechArtPizzaDelivery.Domain.Interfaces;
 
-namespace iTechArtPizzaDelivery.Infrastructure.Repository.Fake
-{
-    public class PizzasFakeRepository : IPizzasRepository
-    {
-        // Default pizzas list
-        private static List<Pizza> _pizzas = new List<Pizza> 
-        {
-            new Pizza
-                (
-                    id: 0,
-                    name: "Banana Pizza",
-                    description: "This is a Banana pizza"
-                ),
+//namespace iTechArtPizzaDelivery.Infrastructure.Repository.Fake
+//{
+//    public class PizzasFakeRepository : IPizzasRepository
+//    {
+//        // Default pizzas list
+//        private static List<Pizza> _pizzas = new List<Pizza> 
+//        {
+//            new Pizza
+//                (
+//                    id: 0,
+//                    name: "Banana Pizza",
+//                    description: "This is a Banana pizza"
+//                ),
 
-                    new Pizza
-                (
-                    id: 1,
-                    name: "Tomato Pizza",
-                    description: "This is a Tomato pizza"
-                ),
+//                    new Pizza
+//                (
+//                    id: 1,
+//                    name: "Tomato Pizza",
+//                    description: "This is a Tomato pizza"
+//                ),
 
-                    new Pizza
-                (
-                    id: 2,
-                    name: "Cheese Pizza",
-                    description: "This is a Cheese pizza"
-                ),
+//                    new Pizza
+//                (
+//                    id: 2,
+//                    name: "Cheese Pizza",
+//                    description: "This is a Cheese pizza"
+//                ),
 
-                    new Pizza
-                (
-                    id: 3,
-                    name: "Apple Pizza",
-                    description: "This is an Apple pizza"
-                ),
-        };
+//                    new Pizza
+//                (
+//                    id: 3,
+//                    name: "Apple Pizza",
+//                    description: "This is an Apple pizza"
+//                ),
+//        };
 
-        public List<Pizza> GetPizzasInfo() => _pizzas;
+//        public List<Pizza> GetAllPizzas() => _pizzas;
 
-        public Pizza FindPizzaById(ulong id)
-        {
-            Pizza lookingPizza = (from pizza in _pizzas
-                                 where (pizza.Id == id)
-                                 select pizza).FirstOrDefault();
+//        public Pizza FindPizzaById(ulong id)
+//        {
+//            Pizza lookingPizza = (from pizza in _pizzas
+//                                 where (pizza.PizzaID == id)
+//                                 select pizza).FirstOrDefault();
 
-            if (lookingPizza != null)
-                return lookingPizza;
-            else
-                return null;
-        }
+//            return lookingPizza;
+//        }
 
-        public void PostPizza(string name, string description)
-        {
-            _pizzas.Add
-            (
-                new Pizza
-                (
-                    id: (ulong)_pizzas.Count, // List length is equal to the next pizzas's ID
-                    name: name,
-                    description: description
-                )
-            );
-        }
-    }
-}
+//        public void PostPizza(string name, string description)
+//        {
+//            _pizzas.Add
+//            (
+//                new Pizza
+//                (
+//                    id: (ulong)_pizzas.Count, // List length is equal to the next pizzas's ID
+//                    name: name,
+//                    description: description
+//                )
+//            );
+//        }
+//    }
+//}
