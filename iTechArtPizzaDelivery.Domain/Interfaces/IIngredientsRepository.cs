e388839ac156ff5c917049654291df58149b797a
the-17th-fox <nikitos171002@yandex.by>
2021-11-04 00:51:16 +0300

    Add ingredients repository interface

---
+++ iTechArtPizzaDelivery.Domain/Interfaces/IIngredientsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PD.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PD.Domain.Interfaces
+{
+    public interface IIngredientsRepository
+    {
+        public Task<List<Ingredient>> GetIngredientsAsync();
+        public Task<Ingredient> GetIngredientAsync(int id);
+        public Task<Ingredient> AddIngredientAsync(string name);
+        public Task<Ingredient> DeleteIngredientAsync(int id);
+    }
+}
