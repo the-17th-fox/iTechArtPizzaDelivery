7e92f71c9bae7db1726cae5bd99bec094eebca86
the-17th-fox <nikitos171002@yandex.by>
2021-11-04 00:49:25 +0300

    Add not implemented addIngredient method

--- iTechArtPizzaDelivery.Domain/Interfaces/IPizzasRepository.cs
+++ iTechArtPizzaDelivery.Domain/Interfaces/IPizzasRepository.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using iTechArtPizzaDelivery.Domain.Entities;
+using PD.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
-namespace iTechArtPizzaDelivery.Domain.Interfaces
+namespace PD.Domain.Interfaces
 {
     public interface IPizzasRepository
     {
         public Task<List<Pizza>> GetPizzasAsync();
         public Task<Pizza> GetPizzaAsync(int id);
-        public Task<Pizza> CreatePizzaAsync(string name, string description);
-        public Task<Pizza> RemovePizzaAsync(int id);
+        public Task<Pizza> AddPizzaAsync(string name, string description);
+        public Task<Pizza> DeletePizzaAsync(int id);
+        public Task<Pizza> AddIngredientToPizzaAsync(int ingredientId, int pizzaId);
     }
 }
--- iTechArtPizzaDelivery.Domain/Services/Pizzas/IPizzasService.cs
+++ iTechArtPizzaDelivery.Domain/Services/Pizzas/IPizzasService.cs
@@ -1,17 +1,18 @@
-﻿using iTechArtPizzaDelivery.Domain.Entities;
+﻿using PD.Domain.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace iTechArtPizzaDelivery.Domain.Services
+namespace PD.Domain.Services
 {
     public interface IPizzasService
     {
         public Task<List<Pizza>> GetPizzasAsync();
         public Task<Pizza> GetPizzaAsync(int id);
         public Task<Pizza> AddPizzaAsync(string name, string description);
-        public Task<Pizza> RemovePizzaAsync(int id);
+        public Task<Pizza> DeletePizzaAsync(int id);
+        public Task<Pizza> AddIngredientToPizzaAsync(int ingredientId, int pizzaId);
     }
 }
