caac5cc7798e30870b4c392f5f2d48a5039375c0
the-17th-fox <nikitos171002@yandex.by>
2021-11-09 13:54:54 +0300

    Add Order entity

---
+++ PD.Domain/Entities/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Entities
+{
+    public class Order
+    {
+        [Key]
+        public int OrderID { get; set; }
+        public int UserID { get; set; }
+        public string Adress { get; set; }
+        public int PromoCodeID { get; set; }
+        public int PizzaID { get; set; }
+    }
+}
