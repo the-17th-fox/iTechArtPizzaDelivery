868551d960d829913c455c2e34ff35be7ff4734c
the-17th-fox <nikitos171002@yandex.by>
2021-11-09 21:04:32 +0300

    Add IPromoCodesRepository interface

---
+++ PD.Domain/Interfaces/IPromoCodesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PD.Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PD.Domain.Interfaces
+{
+    public interface IPromoCodesRepository
+    {
+        public Task<List<PromoCode>> GetPromoCodesAsync();
+        public Task<PromoCode> GetPromoCodeAsync(int id);
+        public Task<PromoCode> AddPromoCodeAsync(string name, string description, float discountAmount);
+        public Task<PromoCode> DeletePromoCodeAsync(int id);
+    }
+}
