f64b94f9370d1e7216f3e68509619ab571a621ca
the-17th-fox <nikitos171002@yandex.by>
2021-11-09 21:05:10 +0300

    Add IPromoCodesService and its implemenation

---
+++ PD.Domain/Services/PromoCodes/IPromoCodesService.cs
@@ -0,0 +1,17 @@
+﻿using PD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Services
+{
+    public interface IPromoCodesService
+    {
+        public Task<List<PromoCode>> GetPromoCodesAsync();
+        public Task<PromoCode> GetPromoCodeAsync(int id);
+        public Task<PromoCode> AddPromoCodeAsync(string name, string description, float discountAmount);
+        public Task<PromoCode> DeletePromoCodeAsync(int id);
+    }
+}
---
+++ PD.Domain/Services/PromoCodes/PromoCodesService.cs
@@ -0,0 +1,25 @@
+﻿using PD.Domain.Entities;
+using PD.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Services
+{
+    class PromoCodesService : IPromoCodesService
+    {
+        private readonly IPromoCodesRepository _repository;
+        public PromoCodesService(IPromoCodesRepository repository) => _repository = repository;
+
+        public async Task<PromoCode> AddPromoCodeAsync(string name, string description, float discountAmount)
+            => await _repository.AddPromoCodeAsync(name, description, discountAmount);
+
+        public async Task<PromoCode> DeletePromoCodeAsync(int id) => await _repository.DeletePromoCodeAsync(id);
+
+        public async Task<PromoCode> GetPromoCodeAsync(int id) => await _repository.GetPromoCodeAsync(id);
+
+        public async Task<List<PromoCode>> GetPromoCodesAsync() => await _repository.GetPromoCodesAsync();
+    }
+}
