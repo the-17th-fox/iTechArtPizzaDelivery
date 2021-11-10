ca18bfbcdd198635db1222f84a541f8987ce0ede
the-17th-fox <nikitos171002@yandex.by>
2021-11-10 15:24:21 +0300

    Add IOrdersRep interface

---
+++ PD.Domain/Interfaces/IOrdersRepository.cs
@@ -0,0 +1,18 @@
+﻿using PD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PD.Domain.Interfaces
+{
+    public interface IOrdersRepository
+    {
+        public Task<List<Order>> GetOrdersAsync();
+        public Task<Order> GetOrderAsync(int id);
+        public Task<Order> AddOrderAsync(int userId, int pizzaId, string adress, int? promoCodeId = null);
+        public Task<Order> DeleteOrderAsync(int id);
+    }
+}
