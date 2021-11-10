801dccae26efde38f9823ac03c755cf0c667fed6
the-17th-fox <nikitos171002@yandex.by>
2021-11-10 15:23:54 +0300

    Add IOrdersService and its implementation

---
+++ PD.Domain/Services/Orders/IOrdersService.cs
@@ -0,0 +1,17 @@
+﻿using PD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Services.Orders
+{
+    public interface IOrdersService
+    {
+        public Task<List<Order>> GetOrdersAsync();
+        public Task<Order> GetOrderAsync(int id);
+        public Task<Order> AddOrderAsync(int userId, int pizzaId, string adress, int? promoCodeId = null);
+        public Task<Order> DeleteOrderAsync(int id);
+    }
+}
---
+++ PD.Domain/Services/Orders/OrdersService.cs
@@ -0,0 +1,25 @@
+﻿using PD.Domain.Entities;
+using PD.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Services.Orders
+{
+    public class OrdersService : IOrdersService
+    {
+        private readonly IOrdersRepository _repository;
+        public OrdersService(IOrdersRepository repository) => _repository = repository;
+
+        public async Task<Order> AddOrderAsync(int userId, int pizzaId, string adress, int? promoCodeId = null)
+            => await _repository.AddOrderAsync(userId, pizzaId, adress, promoCodeId);
+
+        public async Task<Order> DeleteOrderAsync(int id) => await _repository.DeleteOrderAsync(id);
+
+        public async Task<Order> GetOrderAsync(int id) => await _repository.GetOrderAsync(id);
+
+        public async Task<List<Order>> GetOrdersAsync() => await _repository.GetOrdersAsync();
+    }
+}
