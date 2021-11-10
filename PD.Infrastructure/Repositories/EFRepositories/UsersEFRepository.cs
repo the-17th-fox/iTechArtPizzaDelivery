2fd96763dd356566b4550eb10add8397c25d52db
the-17th-fox <nikitos171002@yandex.by>
2021-11-10 19:19:04 +0300

    Add UsersEFRep

---
+++ PD.Infrastructure/Repositories/EFRepositories/UsersEFRepository.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PD.Domain.Entities;
+using PD.Domain.Interfaces;
+using PD.Infrastructure.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Infrastructure.Repositories.EFRepositories
+{
+    public class UsersEFRepository : IUsersRepository
+    {
+        private readonly PizzaDeliveryContext _dbContext;
+        public UsersEFRepository(PizzaDeliveryContext context) => _dbContext = context;
+
+        public async Task<User> GetByIdAsync(int id) => await _dbContext.Users.FindAsync(id);
+
+        public async Task<List<User>> GetAllAsync() => await _dbContext.Users.ToListAsync();
+    }
+}
