6330e2cad9179bc46abf73e9dbed21f55ac6e1b9
the-17th-fox <nikitos171002@yandex.by>
2021-11-10 15:24:59 +0300

    Add nullable type to Order.PromoCodeId property

--- PD.Domain/Entities/Order.cs
+++ PD.Domain/Entities/Order.cs
@@ -13,7 +13,7 @@ namespace PD.Domain.Entities
         public int OrderID { get; set; }
         public int UserID { get; set; }
         public string Adress { get; set; }
-        public int PromoCodeID { get; set; }
+        public int? PromoCodeID { get; set; }
         public int PizzaID { get; set; }
     }
 }
