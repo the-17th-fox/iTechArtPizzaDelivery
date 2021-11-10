8adfe3b2217f4c8af8a85f8325d17a9f28bea5b2
the-17th-fox <nikitos171002@yandex.by>
2021-11-10 19:18:45 +0300

    Add IUsersService and it's methods implementation

---
+++ PD.Domain/Services/Users/UsersService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PD.Domain.Entities;
+using PD.Domain.Interfaces;
+
+namespace PD.Domain.Services
+{
+    public class UsersService : IUsersService
+    {
+        private readonly IUsersRepository _repository;
+        public UsersService(IUsersRepository repository) => _repository = repository;
+
+        public async Task<User> GetByIdAsync(int id) => await _repository.GetByIdAsync(id);
+
+        public async Task<List<User>> GetAllAsync() => await _repository.GetAllAsync();
+    }
+}
