8fe16e7baa29daea4f925d21baaa31e8876be2f4
the-17th-fox <nikitos171002@yandex.by>
2021-11-15 09:06:44 +0300

    Add UserId and PromoCodeId foreign keys in Order entity

--- PD.Domain/Entities/Order.cs
+++ PD.Domain/Entities/Order.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,10 +12,16 @@ namespace PD.Domain.Entities
     {
         [Key]
         public int Id { get; set; }
-        public int UserId { get; set; }
-        public int? PromoCodeId { get; set; }
-        public List<Pizza> Pizzas { get; set; }
+        public virtual List<Pizza> Pizzas { get; set; }
         public string Adress { get; set; }
         public bool IsPaid { get; set; } = false;
+
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        public int? PromoCodeId { get; set; }
+        [ForeignKey("PromoCodeId")]
+        public PromoCode PromoCode { get; set; }
     }
 }
