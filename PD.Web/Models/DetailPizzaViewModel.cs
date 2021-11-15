41f85f21e8bbdc952506f8d1be11efeb945878f3
the-17th-fox <nikitos171002@yandex.by>
2021-11-15 22:08:35 +0300

    Add DetailPizzaViewModel and PizzaViewModel

---
+++ PD.Web/Models/DetailPizzaViewModel.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using PD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PD.Web.Models
+{ 
+    /// <summary>
+    /// GetById
+    /// </summary>
+    public class DetailPizzaViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Ingredient> Ingredients { get; set; }
+    }
+}
