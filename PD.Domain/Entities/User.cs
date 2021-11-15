66ca2199e10b62e3d76185caf826723398ab0f5e
the-17th-fox <nikitos171002@yandex.by>
2021-11-15 09:08:47 +0300

    Add OrderId relation to User entity

--- PD.Domain/Entities/User.cs
+++ PD.Domain/Entities/User.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +12,11 @@ namespace PD.Domain.Entities
     {
         [Key]
         public int Id { get; set; }
-        
         public string Name { get; set; }
-
         public string PhoneNumber { get; set; }
+
+        public int? OrderId { get; set; }
+        [ForeignKey("OrderId")]
+        public Order Order { get; set; }
     }
 }
