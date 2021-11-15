96db3f42e0cb545031a741c6dc160b779eef5fe9
the-17th-fox <nikitos171002@yandex.by>
2021-11-15 09:04:05 +0300

    Add Route with method title in PizzaController

--- PD.Web/Controllers/PizzasController.cs
+++ PD.Web/Controllers/PizzasController.cs
@@ -23,39 +23,40 @@ namespace PD.Domain.Controllers
             _pizzasService = service;
         }
 
-        [ActionName(nameof(GetAllPizzasAsync))]
+        [ActionName(nameof(GetAllAsync))]
         [HttpGet]
-        public async Task<List<Pizza>> GetAllPizzasAsync() => await _pizzasService.GetAllAsync();
+        public async Task<List<Pizza>> GetAllAsync() => await _pizzasService.GetAllAsync();
 
         [Route("{id}")]
         [HttpGet]
         public async Task<Pizza> GetPizzaAsync(int id) => await _pizzasService.GetByIdAsync(id);
 
         [HttpPost]
-        public async Task<ActionResult> AddPizzaAsync(string name, string description)
+        public async Task<ActionResult> AddAsync(string name, string description)
         {
             Pizza newPizza = await _pizzasService.AddAsync(name, description);
 
-            return CreatedAtAction(nameof(GetAllPizzasAsync),
-                new { id = newPizza.PizzaID }, newPizza);
+            return CreatedAtAction(nameof(GetAllAsync),
+                new { id = newPizza.Id }, newPizza);
         }
 
-        [HttpPut]
+        [HttpDelete]
+        public async Task<ActionResult> DeleteAsync(int id)
+        {
+            Pizza pizzaToRemove = await _pizzasService.DeleteAsync(id);
+
+            return CreatedAtAction(nameof(GetAllAsync),
+                    new { id = pizzaToRemove.Id }, pizzaToRemove);
+        }
+
+        [Route("api/[controller]/[action]")]
+        [HttpPut()]
         public async Task<ActionResult> AddIngredientToPizza(int ingredientId, int pizzaId)
         {
             Pizza pizza = await _pizzasService.AddIngredientToPizzaAsync(ingredientId, pizzaId);
 
-            return CreatedAtAction(nameof(GetAllPizzasAsync),
-                    new { id = pizza.PizzaID }, pizza);
-        }
-
-        [HttpDelete]
-        public async Task<ActionResult> DeletePizzaAsync(int id)
-        {
-            Pizza pizzaToRemove = await _pizzasService.DeleteAsync(id);
-
-            return CreatedAtAction(nameof(GetAllPizzasAsync),
-                    new { id = pizzaToRemove.PizzaID }, pizzaToRemove);
+            return CreatedAtAction(nameof(GetAllAsync),
+                    new { id = pizza.Id }, pizza);
         }
     }
 }
