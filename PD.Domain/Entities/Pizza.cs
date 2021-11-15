e4cdb2935bde299301fe105ec870f6744025ef6a
the-17th-fox <nikitos171002@yandex.by>
2021-11-15 22:01:39 +0300

    Remove list initialization and Add orders relationship

--- PD.Domain/Entities/Pizza.cs
+++ PD.Domain/Entities/Pizza.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace PD.Domain.Entities
@@ -11,14 +12,11 @@ namespace PD.Domain.Entities
     {
         [Key]
         public int Id { get; set; }
-
         public string Name { get; set; }
-
         public string Description { get; set; }
-
-        public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
-
+        public List<Ingredient> Ingredients { get; set; }
         //public /*Image*/ Image { get; set; }
+        public List<Order> Orders { get; set; }
 
     }
 }
