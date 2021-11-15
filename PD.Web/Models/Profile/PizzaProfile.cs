477dd8cb89dc80f2663337f92490aac93dccd435
the-17th-fox <nikitos171002@yandex.by>
2021-11-15 22:08:48 +0300

    Add Pizza profile for automapper

---
+++ PD.Web/Models/Profile/PizzaProfile.cs
@@ -0,0 +1,32 @@
+﻿using PD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace PD.Web.Models
+{
+    public class PizzaProfile : Profile
+    {
+        public PizzaProfile()
+        {
+            CreateMap<Pizza, DetailPizzaViewModel>() // GetAll
+                .ForMember(p => p.Id,                           
+                    option => option.MapFrom(src => src.Id)) // ID
+                .ForMember(p => p.Name,
+                    option => option.MapFrom(src => src.Name)) // NAME
+                .ForMember(p => p.Description,
+                    option => option.MapFrom(src => src.Description)) // DESCRIPTION
+                .ForMember(p => p.Ingredients,
+                    option => option.MapFrom(src => src.Ingredients)); // INGREDIENTS list
+
+            CreateMap<Pizza, PizzaViewModel>() // GetById
+                .ForMember(p => p.Id, 
+                    option => option.MapFrom(src => src.Id)) // ID
+                .ForMember(p => p.Name,
+                    option => option.MapFrom(src => src.Name)); // NAME
+        }
+
+    }
+}
