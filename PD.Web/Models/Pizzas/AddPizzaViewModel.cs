723c3f37cfed813e065e024c44c1a93374ec8cc5
the-17th-fox <nikitos171002@yandex.by>
2021-11-17 19:35:28 +0300

    Add AddPizzaViewModel

---
+++ PD.Web/Models/Pizzas/AddPizzaViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PD.Web.Models
+{
+    public class AddPizzaViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
