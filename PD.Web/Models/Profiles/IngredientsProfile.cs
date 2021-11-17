ae8afebe9bb534e05ded9d3ce7b3c781ee70595f
the-17th-fox <nikitos171002@yandex.by>
2021-11-17 19:36:26 +0300

    Add IngredientsProfile

---
+++ PD.Web/Models/Profiles/IngredientsProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using PD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PD.Web.Models
+{
+    public class IngredientsProfile : Profile
+    {
+        public IngredientsProfile()
+        {
+            CreateMap<Ingredient, IngredientViewModel>()
+                .ForMember(i => i.Id,
+                    option => option.MapFrom(src => src.Id)) // ID
+
+                .ForMember(i => i.Name,
+                    option => option.MapFrom(src => src.Name)) // NAME
+
+                .ForMember(i => i.Pizzas,
+                    option => option.MapFrom(src => src.Pizzas)); // PIZZAS list
+
+            CreateMap<Ingredient, ShortIngredientViewModel>()
+                .ForMember(i => i.Id,
+                    option => option.MapFrom(src => src.Id)) // ID
+
+                .ForMember(i => i.Name,
+                    option => option.MapFrom(src => src.Name)); // NAME
+        }
+    }
+}
