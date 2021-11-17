93070ac8ddb3bcfe51a7820882321675e93b0939
the-17th-fox <nikitos171002@yandex.by>
2021-11-17 19:34:01 +0300

    Add short and detailed IngredientViewModel

---
+++ PD.Web/Models/IngredientViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PD.Web.Models
+{
+    public class IngredientViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<ShortPizzaViewModel> Pizzas { get; set; } = new List<ShortPizzaViewModel>();
+    }
+}
