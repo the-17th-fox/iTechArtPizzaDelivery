bd1d9205ff34866abe7c10ee6a8abf1629270fa4
the-17th-fox <nikitos171002@yandex.by>
2021-11-18 18:09:02 +0300

    Add new Order models
    ShortOrderViewModel
    AddOrderViewModel
    AdressInOrderViewModel
    OrderViewModel

---
+++ PD.Web/Models/Order/AdressInOrderViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PD.Web.Models
+{
+    public class AdressInOrderViewModel
+    {
+        public int Id { get; set; }
+        public string Adress { get; set; }
+    }
+}
