253a43c9062152cb2dd5d03c790f2d1a4c3e809d
the-17th-fox <nikitos171002@yandex.by>
2021-12-02 21:57:29 +0300

    Add AuthOptions

---
+++ PD.Domain/Constants/AuthOptions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Constants.AuthOptions
+{
+    public class AuthOptions
+    {
+        public const string ISSUER = "PizzaDeliveryServer";
+        public const string AUDIENCE = "PizzaDeliveryClient";
+        public const int LIFETIME = 1;
+        private const string KEY = "asdfghjklfoxlkjhgfdsa";
+        public static SymmetricSecurityKey GetKey()
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(KEY));
+        }
+    }
+}
