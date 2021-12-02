b033217fa925f4c72e12a2c118094e722f3b9954
the-17th-fox <nikitos171002@yandex.by>
2021-12-02 21:57:47 +0300

    Add OrderStatuses enum

---
+++ PD.Domain/Constants/OrderStatuses.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Constants.OrderStatuses
+{
+    public enum OrderStatuses
+    {
+        CookingInProgress,
+        Delivering,
+        Delivered
+    }
+}
