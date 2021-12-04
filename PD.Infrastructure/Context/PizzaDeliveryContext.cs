2ad2645bf6e196fd06d813ee9118624aee993533
the-17th-fox <nikitos171002@yandex.by>
2021-12-04 14:48:55 +0300

    Add roles OnModelCreating to the PizzaDeliveryContext

--- PD.Infrastructure/Context/PizzaDeliveryContext.cs
+++ PD.Infrastructure/Context/PizzaDeliveryContext.cs
@@ -7,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using PD.Domain.Constants.UsersRoles;
 
 namespace PD.Infrastructure.Context
 {
@@ -19,5 +20,14 @@ namespace PD.Infrastructure.Context
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<PromoCode> PromoCodes { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            List<IdentityRole<long>> usersRoles = RolesNames.GetUsersRoles();
+            modelBuilder.Entity<IdentityRole<long>>().HasData(usersRoles);
+            
+        }
     }
 }
