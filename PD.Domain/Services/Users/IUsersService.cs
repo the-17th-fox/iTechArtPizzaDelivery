5188642c7a286d28965bd2e708f5ed9e962492b7
the-17th-fox <nikitos171002@yandex.by>
2021-12-04 14:48:18 +0300

    Remove implementation of IBaseService interface from IUsersService

--- PD.Domain/Services/Users/IUsersService.cs
+++ PD.Domain/Services/Users/IUsersService.cs
@@ -3,10 +3,11 @@ using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace PD.Domain.Services
 {
-    public interface IUsersService : IBaseService<User>
+    public interface IUsersService
     {
         public JwtSecurityToken GetNewToken(List<Claim> authClaims);
         public List<Claim> GetUserClaims(User user, IList<string> userRoles);
