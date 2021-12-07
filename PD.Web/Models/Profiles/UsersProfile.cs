ad991bf3ade4ae29df619ab429cbf99d5685f379
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2021-12-07 22:41:27 +0300

    Remove Unneccessary map configuration from UsersProfile

--- PD.Web/Models/Profiles/UsersProfile.cs
+++ PD.Web/Models/Profiles/UsersProfile.cs
@@ -11,57 +11,11 @@ namespace PD.Web.Models.Profiles
     {
         public UsersProfile()
         {
-            CreateMap<User, UserViewModel>() // Detailed user's info
-                .ForMember(pc => pc.Id,
-                    option => option.MapFrom(src => src.Id)) // ID
+            CreateMap<User, UserViewModel>().ReverseMap();
+            CreateMap<User, ShortUserViewModel>().ReverseMap();
 
-                .ForMember(pc => pc.FirstName,
-                    option => option.MapFrom(src => src.FirstName)) // FIRST NAME
-
-                .ForMember(pc => pc.LastName,
-                    option => option.MapFrom(src => src.LastName)) // LAST NAME
-
-                .ForMember(pc => pc.Email,
-                    option => option.MapFrom(src => src.Email)) // EMAIL
-
-                .ForMember(pc => pc.Order,
-                    option => option.MapFrom(src => src.Order)) // ORDER
-                .ReverseMap();
-
-
-            CreateMap<User, ShortUserViewModel>() // Short user's info
-                .ForMember(pc => pc.Id,
-                    option => option.MapFrom(src => src.Id)) // ID
-
-                .ForMember(pc => pc.Email,
-                    option => option.MapFrom(src => src.Email)) // EMAIL
-
-                .ForMember(pc => pc.Order,
-                    option => option.MapFrom(src => src.Order)) // ORDER
-                .ReverseMap();
-
-
-            CreateMap<User, RegisterUserModel>() // Register model
-
-                .ForMember(pc => pc.FirstName,
-                    option => option.MapFrom(src => src.FirstName)) // FIRST NAME
-
-                .ForMember(pc => pc.LastName,
-                    option => option.MapFrom(src => src.LastName)) // LAST NAME
-
-                .ForMember(pc => pc.Email,
-                    option => option.MapFrom(src => src.UserName)) // EMAIL
-
-                .ForMember(pc => pc.PhoneNumber,
-                    option => option.MapFrom(src => src.PhoneNumber)) // PHONE NUM
-                .ReverseMap();
-
-
-            CreateMap<User, LoginUserModel>() // Login model
-
-                .ForMember(pc => pc.Email,
-                    option => option.MapFrom(src => src.NormalizedEmail)) // EMAIL
-                .ReverseMap();
+            CreateMap<User, LoginUserModel>().ReverseMap();
+            CreateMap<User, RegisterUserModel>().ReverseMap();
         }
     }
 }
