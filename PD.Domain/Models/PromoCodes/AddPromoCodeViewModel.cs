09f699ee9a861610e493f427e7f1bc26d8e18527
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2021-12-19 23:31:15 +0300

    Add validation attributes to the AddPromoCodeViewModel

--- PD.Domain/Models/PromoCodes/AddPromoCodeViewModel.cs
+++ PD.Domain/Models/PromoCodes/AddPromoCodeViewModel.cs
@@ -8,10 +8,16 @@ namespace PD.Domain.Models
 {
     public class AddPromoCodeViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(maximumLength: 10, ErrorMessage = "The length of the string must not be more than 10 characters")]
         public string Name { get; set; }
-        public string Description { get; set; }
-        [Required]
+
+        [Required(ErrorMessage = "Discount Amount is Required")]
+        [Range(0, 100.0, ErrorMessage = "The value must be in the range from 0 to 100")]
         public float DiscountAmount { get; set; }
+
+        [StringLength(maximumLength: 150, ErrorMessage = "The length of the string must not be more than 150 characters")]
+        public string Description { get; set; }
+        
     }
 }
