60b04db756d43efd4877b44f23f827254b260ccc
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2021-12-19 23:32:33 +0300

    Add exceptions handlers to the PromoCodesService

--- PD.Domain/Services/PromoCodes/PromoCodesService.cs
+++ PD.Domain/Services/PromoCodes/PromoCodesService.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using PD.Domain.Entities;
 using PD.Domain.Interfaces;
 using PD.Domain.Models;
@@ -20,28 +22,55 @@ namespace PD.Domain.Services
             _mapper = mapper;
         }
 
-        public async Task<PromoCodeViewModel> AddAsync(AddPromoCodeViewModel model)
-        { 
-            PromoCode promoCode = await _repository.AddAsync(model);
-            return _mapper.Map<PromoCodeViewModel>(promoCode);
+        public async Task<IActionResult> AddAsync(AddPromoCodeViewModel model)
+        {
+            var existingPromoCode = await _repository.GetByNameAsync(model.Name);
+            // Checks if there is any promocode with the same name
+            if (existingPromoCode != null)
+                return new BadRequestObjectResult("There is already a promo code with this name");
+
+            var promoCode = _mapper.Map<AddPromoCodeViewModel, PromoCode>(model);
+            var result = await _repository.AddAsync(promoCode);
+            // Checks whether the adding was successful
+            if (result == null)
+                return new ObjectResult("An error occured while trying to add a new promocode");
+
+            return new OkObjectResult("The promocode has been added successfully");
         }
 
-        public async Task<PromoCodeViewModel> DeleteAsync(long id)
+        public async Task<IActionResult> DeleteAsync(long id)
         {
-            PromoCode promoCode = await _repository.DeleteAsync(id);
-            return _mapper.Map<PromoCodeViewModel>(promoCode);
+            var promoCode = await _repository.GetByIdAsync(id);
+            // Checks if there is any promocode with the specified ID
+            if (promoCode == null)
+                return new BadRequestObjectResult("The promocode with the specified id does not exist");
+
+            var result = await _repository.DeleteAsync(promoCode);
+            // Checks whether the deletion was successful
+            if(result == null)
+                return new ObjectResult("An error occured while trying to delete the promocode");
+
+            return new OkObjectResult("The promocode has been deleted successfully");
         }
 
-        public async Task<List<ShortPromoCodeViewModel>> GetAllAsync()
+        public async Task<IActionResult> GetAllAsync()
         {
-            List<PromoCode> promoCodes = await _repository.GetAllAsync();
-            return _mapper.Map<List<ShortPromoCodeViewModel>>(promoCodes);
+            var promoCodes = await _repository.GetAllAsync();
+            // Checks if there is any promocodes 
+            if (promoCodes.IsNullOrEmpty())
+                return new ObjectResult("No promocodes were found");
+
+            return new OkObjectResult(_mapper.Map<List<ShortPromoCodeViewModel>>(promoCodes));
         }
 
-        public async Task<PromoCodeViewModel> GetByIdAsync(long id)
+        public async Task<IActionResult> GetByIdAsync(long id)
         {
-            PromoCode promoCode = await _repository.GetByIdAsync(id);
-            return _mapper.Map<PromoCodeViewModel>(promoCode);
+            var promoCode = await _repository.GetByIdAsync(id);
+            // Checks if there is any promocode with the specified ID    
+            if (promoCode == null)
+                return new NotFoundObjectResult("Promocode was not found");
+
+            return new OkObjectResult(_mapper.Map<PromoCodeViewModel>(promoCode));
         }
     }
 }
