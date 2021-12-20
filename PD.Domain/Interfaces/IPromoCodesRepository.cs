295c917cd1c3fd3f5a403686c2ccdf4aae831ac4
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2021-12-20 22:48:57 +0300

    Add Existance check methods to the IPromoCodeRep

--- PD.Domain/Interfaces/IPromoCodesRepository.cs
+++ PD.Domain/Interfaces/IPromoCodesRepository.cs
@@ -14,7 +14,21 @@ namespace PD.Domain.Interfaces
         public Task<List<PromoCode>> GetAllAsync();
         public Task<PromoCode> GetByIdAsync(long id);
         public Task<PromoCode> AddAsync(PromoCode promoCode);
-        public Task<PromoCode> DeleteAsync(PromoCode promoCode);
+        public Task<PromoCode> DeleteAsync(long id);
         public Task<PromoCode> GetByNameAsync(string name);
+
+        /// <summary>
+        /// Searchs for the promocode in the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns true if the promocode with the specified ID exists</returns>
+        public Task<bool> ExistsAsync(long id);
+
+        /// <summary>
+        /// Searchs for the promocode in the database
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Returns true if the promocode with the specified name exists</returns>
+        public Task<bool> ExistsAsync(string name);
     }
 }
