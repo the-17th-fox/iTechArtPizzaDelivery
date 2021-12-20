a272c3c5b9ed35c7326134d1af4560727889d382
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2021-12-20 22:51:26 +0300

    Add Existance check methods to the IIngredientsRep

--- PD.Domain/Interfaces/IIngredientsRepository.cs
+++ PD.Domain/Interfaces/IIngredientsRepository.cs
@@ -12,7 +12,21 @@ namespace PD.Domain.Interfaces
     {
         public Task<List<Ingredient>> GetAllAsync();
         public Task<Ingredient> GetByIdAsync(long id);
-        public Task<Ingredient> AddAsync(AddIngredientViewModel model);
+        public Task<Ingredient> AddAsync(Ingredient ingredient);
         public Task<Ingredient> DeleteAsync(long id);
+
+        /// <summary>
+        /// Searchs for the ingredient in the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Returns true if the ingredient with the specified ID exists</returns>
+        public Task<bool> ExistsAsync(long id);
+
+        /// <summary>
+        /// Searchs for the ingredient in the database
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns>Returns true if the ingredient with the specified name exists</returns>
+        public Task<bool> ExistsAsync(string name);
     }
 }
