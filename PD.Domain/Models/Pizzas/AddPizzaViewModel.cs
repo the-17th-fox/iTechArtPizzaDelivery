a76c7091874ece18a8fa5b5f6fe9262eabc2a5c7
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2021-12-20 22:45:01 +0300

    Add validation attributes to the AddPizzaViewModel

--- PD.Domain/Models/Pizzas/AddPizzaViewModel.cs
+++ PD.Domain/Models/Pizzas/AddPizzaViewModel.cs
@@ -9,8 +9,11 @@ namespace PD.Domain.Models
 {
     public class AddPizzaViewModel
     {
-        [Required]
+        [Required(ErrorMessage = "Name is Required")]
+        [StringLength(maximumLength: 20, ErrorMessage = "The length of the string must not be more than 20 characters")]
         public string Name { get; set; }
+
+        [StringLength(maximumLength: 150, ErrorMessage = "The length of the string must not be more than 150 characters")]
         public string Description { get; set; }
     }
 }
