9f2731053f1b4684c706df34f7edfc24936a742a
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-01-11 16:51:54 +0300

    Specify folders for the Constants classes

--- PD.Domain/Constants/DeliveryMethods.cs
+++ PD.Domain/Constants/DeliveryMethods.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PD.Domain.Constants
+namespace PD.Domain.Constants.DeliveryMethods
 {
     public enum DeliveryMethods
     {
