abd7a683c7a74edc7118d32cc8ac8131fcec4f98
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-01-15 21:28:47 +0300

    Add custom PizzaInOrder table, add Pizzas and Ingredients seeding

--- PD.Infrastructure/Context/PizzaDeliveryContext.cs
+++ PD.Infrastructure/Context/PizzaDeliveryContext.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
 using PD.Domain.Constants.UsersRoles;
+using PD.Domain.Constants.ForcedDBTablesData;
 
 namespace PD.Infrastructure.Context
 {
@@ -20,23 +21,27 @@ namespace PD.Infrastructure.Context
         public DbSet<Ingredient> Ingredients { get; set; }
         public DbSet<PromoCode> PromoCodes { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<IngredientPizza> IngredientsPizzas {get;set;}
+        public DbSet<PizzaOrder> PizzasOrders { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
             List<IdentityRole<long>> usersRoles = RolesNames.GetRolesNames();
-            modelBuilder.Entity<IdentityRole<long>>().HasData(usersRoles);
+
+            modelBuilder.Entity<IdentityRole<long>>()
+                .HasData(usersRoles);
 
             modelBuilder
                 .Entity<Order>()
                 .HasMany(o => o.Pizzas)
                 .WithMany(p => p.Orders)
-                .UsingEntity<PizzaInOrder>
+                .UsingEntity<PizzaOrder>
                 (
                     j => j
                         .HasOne(pt => pt.Pizza)
-                        .WithMany(t => t.PizzasInOrders)
+                        .WithMany(t => t.PizzaInOrders)
                         .HasForeignKey(pt => pt.PizzaId),
 
                     j => j
@@ -48,10 +53,51 @@ namespace PD.Infrastructure.Context
                     {
                         j.Property(pt => pt.Amount).HasDefaultValue(1);
                         j.HasKey(t => new { t.OrderId, t.PizzaId });
-                        j.ToTable("PizzasInOrders");
+                        j.ToTable("PizzaOrder");
                     }
                 );
 
+            var pizzas = ForcedDBTablesData.GetPizzas();
+            var ingredients = ForcedDBTablesData.GetIngredients();
+            var pizzaIngredients = new List<object>
+            {
+                new { PizzaId = 1l, IngredientId = 1l },
+                new { PizzaId = 1l, IngredientId = 3l },
+                new { PizzaId = 2l, IngredientId = 2l },
+                new { PizzaId = 2l, IngredientId = 3l },
+                new { PizzaId = 3l, IngredientId = 4l },
+                new { PizzaId = 3l, IngredientId = 3l }
+            };
+
+            modelBuilder
+                .Entity<Pizza>()
+                .HasData(pizzas);
+
+            modelBuilder
+                .Entity<Ingredient>()
+                .HasData(ingredients);
+
+            modelBuilder
+                .Entity<Pizza>()
+                .HasMany(p => p.Ingredients)
+                .WithMany(i => i.Pizzas)
+                .UsingEntity<IngredientPizza>
+                (
+                    ip => ip
+                        .HasOne(pt => pt.Ingredient)
+                        .WithMany(t => t.IngredientInPizzas)
+                        .HasForeignKey(pt => pt.IngredientId),
+
+                    ip => ip
+                        .HasOne(pt => pt.Pizza)
+                        .WithMany(t => t.IngredientsInPizza)
+                        .HasForeignKey(pt => pt.PizzaId)
+                ).ToTable(nameof(IngredientPizza))
+                .HasKey(ip => new { ip.PizzaId, ip.IngredientId });
+
+            modelBuilder
+                .Entity<IngredientPizza>()
+                .HasData(pizzaIngredients);
         }
     }
 }
