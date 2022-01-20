c0b316832f7a3efa4fddefa98f3f7b6e11fc807f
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-01-20 16:36:09 +0300

    Rename Constants file, remove unnecessary property

---
+++ PD.Domain/Constants/FilesStorageConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Constants
+{
+    public class FilesStorageConstants
+    {
+        public const string FilesStoragePath = @"PD.Infrastructure\Files";
+    }
+}
