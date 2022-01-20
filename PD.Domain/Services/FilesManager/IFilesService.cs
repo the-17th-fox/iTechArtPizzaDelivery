d57ec36af792029ccd1616931b1f878149aefb68
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-01-20 16:42:48 +0300

    Make LoadFile method return FileStream + FileType instead of file path + file type

--- PD.Domain/Services/FilesManager/IFilesService.cs
+++ PD.Domain/Services/FilesManager/IFilesService.cs
@@ -2,6 +2,7 @@
 using PD.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace PD.Domain.Services
 {
     public interface IFilesService
     {
-        public Task<FileViewModel> LoadFileAsync(string fileName);
-        public Task<string> DeleteFileAsync(string fileName);
+        public FileViewModel LoadFileAsync(string fileName);
+        public string DeleteFileAsync(string fileName);
     }
 }
