c2955300075675c585461cc648b1b23f89f323d5
the-17th-fox <82900624+the-17th-fox@users.noreply.github.com>
2022-01-26 21:27:16 +0300

    Split AddPizza method into two depending on PizzasInOrder

--- PD.Domain/Interfaces/IOrdersRepository.cs
+++ PD.Domain/Interfaces/IOrdersRepository.cs
@@ -17,18 +17,19 @@ namespace PD.Domain.Interfaces
         public Task<Order> GetEditingReadyAsync(long userId);
         public Task<List<Order>> GetAllFromUserAsync(long userId);
 
-        public Task AddAsync(Order order);
+        public Task<Order> AddAsync(long userId);
         public Task DeleteAsync(Order order);
 
         public Task UpdateIsActiveStatusAsync(Order order, bool status);
-        public Task UpdateOrderStatusAsync(Order order, int statusId);
+        public Task<Order> UpdateOrderStatusAsync(Order order, int statusId);
         public Task UpdateDeliveryMethodAsync(Order order, int methodId);
         public Task UpdateDescriptionAsync(Order order, string newDescription);
         public Task UpdatePromoCodeAsync(Order order, PromoCode promoCode);
         public Task UpdateAdressAsync(Order order, string adress);
 
-        public Task AddPizzaAsync(Order order, Pizza pizza, int numOfPizzasToAdd = 1);
-        public Task RemovePizzaAsync(Order order, Pizza pizza, int numOfPizzasToRemove = 1);
+        public Task<Order> AddNotIncludedPizzaAsync(Order order, Pizza pizza, int numOfPizzasToAdd = 1);
+        public Task<Order> AddIncludedPizza(Order order, Pizza pizza, int numOfPizzasToAdd = 1);
+        public Task<Order> RemovePizzaAsync(Order order, Pizza pizza, int numOfPizzasToRemove = 1);
 
         public int GetSpecifiedPizzaAmount(Order order, Pizza pizza);
     }
