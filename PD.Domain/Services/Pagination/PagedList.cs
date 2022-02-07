7a4a9097de8853da3473cd976b1aadf6c14667c0
the-17th-fox <nikitos171002@yandex.by>
2022-02-07 16:19:51 +0300

    Remove unnecessary calculation, fix bug with incorrect Prev/Next showing on PageViewModel

--- PD.Domain/Services/Pagination/PagedList.cs
+++ PD.Domain/Services/Pagination/PagedList.cs
@@ -13,9 +13,29 @@ namespace PD.Domain.Services.Pagination
         public int TotalPages { get; private set; }
         public int PageSize { get; private set; }
         public int TotalItemsCount { get; private set; }
+        public bool HasPreviousPage
+        {
+            get
+            {
+                if (TotalPages != 0
+                        && CurrentPage > 1
+                        && CurrentPage <= TotalPages) 
+                    return true;
 
-        public bool HasPreviousPage => CurrentPage > 1;
-        public bool HasNextPage => CurrentPage < TotalPages;
+                else return false;
+            }
+        }
+        public bool HasNextPage
+        {
+            get
+            {
+                if (TotalPages != 0 
+                        && CurrentPage < TotalPages) 
+                    return true;
+
+                else return false;
+            }
+        }
 
         public PagedList(List<T> items, int totalPages, int itemsCount, int pageNumber, int pageSize)
         {
@@ -32,7 +52,6 @@ namespace PD.Domain.Services.Pagination
             var itemsCount = query.Count();
 
             int totalPages = (int)Math.Ceiling(itemsCount / (double)pageSize);
-            pageNumber = (pageNumber > totalPages) ? totalPages : pageNumber;
 
             var items = query
                 .Skip((pageNumber - 1) * pageSize)
