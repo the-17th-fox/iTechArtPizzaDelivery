a043f89a4c257230a34cba2c5c48c75183c6a6ae
the-17th-fox <nikitos171002@yandex.by>
2022-02-07 19:31:06 +0300

    Add viewmodels to change password and names, also model to show new names

---
+++ PD.Domain/Models/Users/ChangeNamesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Models
+{
+    public class ChangeNamesViewModel
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+    }
+}
---
+++ PD.Domain/Models/Users/ChangePasswordViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD.Domain.Models
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Email is Required")]
+        [EmailAddress(ErrorMessage = "Email is Incorrect")]
+        [StringLength(maximumLength: 30, MinimumLength = 5, ErrorMessage = "The length of the string must be in the range from 5 to 30 characters")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(5, ErrorMessage = "The length of the string must be more than 5 characters")]
+        public string OldPassword { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(5, ErrorMessage = "The length of the string must be more than 5 characters")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Password is Required")]
+        [MinLength(5, ErrorMessage = "The length of the string must be more than 5 characters")]
+        public string ConfirmNewPassword { get; set;}
+    }
+}
